4cd71c9d54793029764f278b082ed232ec02d534
x3776x <abrahamvvquinto@gmail.com>
2025-10-08 15:16:41 -0600

    add env file for pw management

--- backend/auth-service/app/database.py
+++ backend/auth-service/app/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://papu:CocteauTwins@postgres_db:5432/pasadena_db")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 # Create engine and session
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
